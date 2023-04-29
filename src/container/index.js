9d9375d6b36872c9a03c8efa6e89d7860e99c843
Adil Ahamed <adilahamed189@gmail.com>
2023-04-29 20:15:04 +0530

    first commit

---
+++ src/container/index.js
@@ -0,0 +1,5 @@
+export { default as About } from './About/About';
+export { default as Explore } from './Explore/Explore';
+export { default as Header } from './Header/Header';
+export { default as Products } from './Products/Products';
+export { default as Contact } from './Contact/Contact';
